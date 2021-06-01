28cd8f622f8dc41b9e6bafe64d02dcaa647aaf6b
HCaetano <hrcgeo@gmail.com>
2021-06-01 18:25:15 -0300

    feat: implement function to find both smallest and biggest number

--- 02-min-max/main.c
+++ 02-min-max/main.c
@@ -1,21 +1,28 @@
-// struct disciplina Aluno matriculados tem um asterisco
-
 #include <stdio.h>
-#include <string.h>
 
-void menor(int* numeros, int arrayLength, int *numeroComparado);
-int maior(int* numeros, int numeroComparado);
+void menor(int* numeros, int arrayLength, int *menorNumero);
+void maior(int* numeros, int arrayLength, int *maiorNumero);
+void menorMaior(int* numeros, int arrayLength, int *menorNumero, int *maiorNumero);
 
 int main()
 {
 
   int numeros[5] = { 4, 7, 1, 10, -1 };
   int arrayLength = sizeof(numeros)/sizeof(numeros[0]);
+
   int menorNumero = 999999;
   menor(numeros, arrayLength, &menorNumero);
+  printf("menor número: %d\n", menorNumero);
 
-  printf("menorNumero: %d\n", menorNumero);
+  int maiorNumero = -999999;
+  maior(numeros, arrayLength, &maiorNumero);
+  printf("maior número: %d\n", maiorNumero);
 
+  int outroMenor = 999999;
+  int outroMaior = -999999;
+  menorMaior(numeros, arrayLength, &outroMenor, &outroMaior);
+  printf("outro menor: %d\n", outroMenor);
+  printf("outro maior: %d\n", outroMaior);
 }
 
 void menor(int* numeros, int arrayLength, int *menorNumero)
@@ -29,15 +36,20 @@ void menor(int* numeros, int arrayLength, int *menorNumero)
   }
 }
 
+void maior(int* numeros, int arrayLength, int *maiorNumero)
+{
+  for (int i = 0; i < arrayLength; i++) {
+    int numeroAtual = numeros[i];
+    if (numeroAtual > *maiorNumero)
+    {
+      *maiorNumero = numeroAtual;
+    }
+  }
+}
 
+void menorMaior(int* numeros, int arrayLength, int *menorNumero, int *maiorNumero)
+{
+  menor(numeros, arrayLength, menorNumero);
+  maior(numeros, arrayLength, maiorNumero);
+}
 
-  //  Declare smallest as integer
-  //  Set smallest to 0
-  //  FOR EACH value in A DO
-  //     IF A[n] is less than smallest THEN
-  //        smallest ← A[n]
-  //     ENDIF
-  //  END FOR
-  //  Display smallest
-  
-// }
